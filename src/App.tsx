57ab0ed657d6279633993e07c4df9880fec29dae
Leandro Aranguez <leandro-aranguez@hotmail.com>
2023-02-16 01:24:01 -0300

    delete unnecesary code in app tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
-// import reactLogo from "./assets/react.svg";
 import './App.css'
 
 function App() {
-  return <div className="App"></div>
+  return <div className="App">Hello world</div>
 }
 
 export default App
