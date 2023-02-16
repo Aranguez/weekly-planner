f8be007757de64470b0eb4f3f86e1efa768f4aae
Leandro Aranguez <leandro-aranguez@hotmail.com>
2023-02-16 01:59:52 -0300

    chore: tadd ests config with next js

--- vitest.config.js
+++ vitest.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vitest/config'
+
+// plugins
+import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
   test: {
     environment: 'happy-dom'
-  },
-  plugins: [tsconfigPaths()]
+  }
 })
